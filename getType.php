6b4c91cfe8a7f0ef0f3b005c71627ff133bbe1d8
ta1dora <taichi.business.hirota@gmail.com>
2022-09-27 18:29:15 +0900

    first commit

---
+++ getType.php
@@ -0,0 +1,12 @@
+<?php
+
+$str = "文字列です";
+$int_number = 4;
+$bool =true;
+$arr = array(1,2,3,4);
+$null =NULL;
+echo gettype($str). PHP_EOL;
+echo gettype($int_number). PHP_EOL;
+echo gettype($bool). PHP_EOL;
+echo gettype($arr).PHP_EOL;
+echo gettype($null). PHP_EOL;
